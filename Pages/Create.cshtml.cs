7717d0a343598d62504bc3883e5bf66eafdde139
Hakim Ghods <ghods.hakim@hotmail.com>
2022-12-02 20:26:41 +0330

    create bug

---
+++ Pages/Create.cshtml.cs
@@ -0,0 +1,35 @@
+using BugNet.Data;
+using BugNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugNet.Pages;
+
+public class CreateModel : PageModel
+{
+    private readonly BugDbContext _db;
+
+    public CreateModel(BugDbContext db)
+    {
+        _db = db;
+    }
+
+    [BindProperty]
+    public string Name { get; set; }
+
+    [BindProperty]
+    public string Description { get; set; }
+    public IActionResult OnPost()
+    {
+        _db.Bugs.Add(new Bug
+        {
+            Name = Name,
+            Description = Description,
+            IsDone=false
+        });
+
+        _db.SaveChanges();
+
+        return RedirectToPage("./index");
+    }
+}
