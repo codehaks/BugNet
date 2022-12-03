fd78ba54b372f7f9045840268e08ac80e5884807
Hakim Ghods <ghods.hakim@hotmail.com>
2022-12-03 17:47:04 +0330

    edit page

--- Pages/Edit.cshtml.cs
+++ Pages/Edit.cshtml.cs
@@ -9,10 +9,29 @@ public class EditModel : PageModel
 {
     private readonly BugDbContext _db;
 
+    public EditModel(BugDbContext db)
+    {
+        _db = db;
+    }
+
+    [BindProperty]
     public Bug? SelectedBug { get; set; }
 
     public void OnGet(int id)
     {
         SelectedBug = _db.Bugs.Find(id);
+    }
+
+    public IActionResult OnPost()
+    {
+        var bug = _db.Bugs.Find(SelectedBug.Id);
+
+        bug.Name = SelectedBug.Name;
+        bug.Description = SelectedBug.Description;
+        bug.IsDone = SelectedBug.IsDone;
+
+        _db.SaveChanges();
+
+        return RedirectToPage("./index");
     }
 }
